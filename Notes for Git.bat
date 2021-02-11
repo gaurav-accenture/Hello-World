ececf55f97565b0219f7128ed4c776e9b24d3141
shivani-girish-devi <shivani.girish.devi@accenture.com>
2021-02-11 12:30:10 +0530

    This is the commit command

---
+++ Notes for Git.bat
@@ -0,0 +1,7 @@
+Notes for Git
+
+git branch  => To display list of all known  branches.
+            => Whenever a branch resides on local machine unless their is a sync, other developers cannot see it.
+git checkout -b featurebranch 
+             => Creates the branch and switches to the same.
+git status => shows the changes in all the  file that have been modified.
